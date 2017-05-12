2615c8958d62a2431f5b9b43973a81e2238758c1
Oliver Mueller <oliver.mueller@esrlabs.com>
2017-05-12 15:56:54 +0200

    some cleanup

--- js/demo.js
+++ js/demo.js
@@ -3,25 +3,12 @@
  * @author Jonathan Nicol - @mrjnicol
  */
 
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
-
-
-/*
-// jQuery plugin example:
 $(document).ready(function() {
   $('#particles').particleground({
     dotColor: '#5cbdaa',
     lineColor: '#5cbdaa'
   });
   $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
+    'margin-top': -($('.intro').height() / 2) + 'px'
   });
 });
-*/
